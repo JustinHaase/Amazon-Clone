ad29ef27b46ae9d8ef19cd77dcd06af5035dd65d
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-02-22 21:28:35 -0500

    added styling and worked on js

--- web/amazon.js
+++ web/amazon.js
@@ -1,8 +1,19 @@
-const  floatingMenu = document.getElementByClass("title");
+const  floatingMenu = document.getElementById("title");
 
-document.getElementByClass("title").addEventListener("title", function() {
-    document.getElementByClass("header").style.color = "green";
+document.getElementById("title").addEventListener("mouseover", function() {
+    document.getElementById("title").style.color = "green";
 });
-document.getElementByClass("title").addEventListener("mouseout", function() {
-     document.getElementByClass("title").style.color = "red";
+document.getElementById("title").addEventListener("mouseout", function() {
+     document.getElementById("title").style.color = "red";
 });
+
+const colorThief = new ColorThief();
+const img = document.querySelector('img');
+
+if (img.complete) {
+  colorThief.getColor(img);
+} else {
+  image.addEventListener('load', function() {
+    colorThief.getColor(img);
+  });
+}
