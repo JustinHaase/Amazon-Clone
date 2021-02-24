584f5c9936ad202103356e0e2ee5ff79da8e456b
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-02-23 22:40:04 -0500

    populated card data through js

--- web/amazon.js
+++ web/amazon.js
@@ -7,13 +7,39 @@ document.getElementById("title").addEventListener("mouseout", function() {
      document.getElementById("title").style.color = "red";
 });
 
-const colorThief = new ColorThief();
-const img = document.querySelector('img');
+// const colorThief = new ColorThief();
+// const img = document.querySelector('img');
 
-if (img.complete) {
-  colorThief.getColor(img);
-} else {
-  image.addEventListener('load', function() {
-    colorThief.getColor(img);
-  });
+// if (img.complete) {
+//   colorThief.getColor(img);
+// } else {
+//   image.addEventListener('load', function() {
+//     colorThief.getColor(img);
+//   });
+// }
+
+const container = document.querySelector(".container");
+
+function showItems(){
+
+let card = document.createElement('div');
+card.className = "card";
+let cardInnerHtml =
+`
+        <div class="image">
+            <img src="BYSKETCHYTANK.png" alt="T-Shirt">
+        </div>
+        <div class="title">
+            <h2>Sketchy Tank T-shirt</h2>
+        </div>
+        <div class="des">
+            <p>$24.99</p>
+            <button><b>Add to cart</b></button>
+        </div>
+`
+card.innerHTML = cardInnerHtml;
+
+return card;
 }
+
+container.appendChild(showItems());
