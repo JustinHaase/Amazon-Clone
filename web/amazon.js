9c3d2deb6553896b9533f0f90c65f5e6d6c73173
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-03-02 14:41:34 -0500

    adjusted sizing, styling, layout and added images

--- web/amazon.js
+++ web/amazon.js
@@ -18,65 +18,66 @@ document.getElementById("title").addEventListener("mouseout", function() {
 //   });
 // }
 
+
 const data = [{
   img: src='img src="1998BK.png" alt="T-Shirt"',
   price: "$24.99",
   description: "T-Shirt",
   button: "Add to Cart"
 },{
-  img: src='img src="1997BK.png" alt="T-Shirt"',
+  img: src='img src="1997BK.png" alt="Long-Sleeve Shirt"',
   price: "$34.99",
   description: "Long-Sleeve Shirt",
   button: "Add to Cart"
 },{
-  img: src='img src="1942BK.png" alt="T-Shirt"',
+  img: src='img src="1942BK.png" alt="Long-Sleeve Shirt""',
   price: "$4.99",
   description: "Long-Sleeve Shirt",
   button: "Add to Cart"
 },{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
+  img: src='img src="1924BK.png" alt="T-Shirt"',
   price: "$24.99",
   description: "T-Shirt",
   button: "Sold Out"
 },{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
+  img: src='img src="1900BK.jpg" alt="Thermal Puff Jacket"',
+  price: "$64.99",
+  description: "Thermal Puff Jacket",
+  button: "Add to Cart"
+},{
+  img: src='img src="1901BK.webp" alt="T-Shirt"',
   price: "$24.99",
   description: "T-Shirt",
   button: "Add to Cart"
 },{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
-  price: "$24.99",
-  description: "T-Shirt",
+  img: src='img src="1902BK.jpg" alt="Thermal Pipe Gloves"',
+  price: "$19.99",
+  description: "Thermal Pipe Gloves",
   button: "Add to Cart"
 },{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
-  price: "$24.99",
-  description: "T-Shirt",
+  img: src='img src="1903BK.webp" alt="Blacklight Hoodie"',
+  price: "$34.99",
+  description: "Blacklight Hoodie",
   button: "Add to Cart"
 },{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
-  price: "$24.99",
-  description: "T-Shirt",
-  button: "Add to Cart"
-},{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
+  img: src='img src="1904BK.webp" alt="T-Shirt"',
   price: "$24.99",
   description: "T-Shirt",
   button: "Sold Out"
 },{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
-  price: "$24.99",
-  description: "T-Shirt",
+  img: src='img src="1905BK.jpg" alt="Bonded Hood"',
+  price: "$19.99",
+  description: "Bonded Hood",
   button: "Add to Cart"
 },{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
-  price: "$24.99",
-  description: "T-Shirt",
+  img: src='img src="1906BK.webp" alt="Fleece Bandana"',
+  price: "$14.99",
+  description: "Fleece Bandana",
   button: "Sold Out"
 },{
-  img: src='img src="1998BK.png" alt="T-Shirt"',
+  img: src='img src="1907BK.webp" alt="Blacklight T-Shirt"',
   price: "$24.99",
-  description: "T-Shirt",
+  description: "Blacklight T-Shirt",
   button: "Add to Cart"
 }
 
@@ -98,8 +99,8 @@ const data = [{
   `
           <div class="card">
           <div class="image"><${result.img} alt="T-Shirt"></div>
-          <div class="title"><h2>${result.description}</h2></div>
           <div class="des">
+          <div class="title"><h2>${result.description}</h2></div>
               <p>${result.price}</p>
               <button><b>${result.button}</b></button>
           </div>
@@ -108,27 +109,4 @@ const data = [{
   container.innerHTML += cardInnerHtml;
 
   })
-  
-  
-
-  
-  // const container = document.querySelector(".container");
-  // const card = document.createElement('div');
-
-  // data.forEach((result)=>{
-  
-  // let card = document.createElement('div');
-  // card.className = "card";
-  // let cardInnerHtml =
-  // `
-  //         <div class="image"><${result.img} alt="T-Shirt"></div>
-  //         <div class="title"><h2>${result.description}</h2></div>
-  //         <div class="des">
-  //             <p>${result.price}</p>
-  //             <button><b>${result.button}</b></button>
-  //         </div>
-  // `
-  // container.innerHTML += cardInnerHtml;
-
-  // })
   
